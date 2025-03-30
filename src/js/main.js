f60fb53db3dd32fbbc1d4037867fff78657a5709
WindyBindy <134945382+WindyBindy@users.noreply.github.com>
2025-03-30 19:20:28 +0200

    new header

--- src/js/main.js
+++ src/js/main.js
@@ -5,5 +5,6 @@ const main = document.getElementById('main')
 btnClear.onclick = function () {
 	header.classList.toggle('header__isHidden')
 	main.classList.toggle('main-ishiden')
+	btnClear.classList.toggle('header__clearbttnhiden')
 }
 const btnBack = document.getElementById('btnBack')
